d92a09b2decc4c750dfaa2ee787055a9ee802c34
mizoguch-ken <mizoguch.ken@gmail.com>
2017-09-17 14:13:11 +0900

    fix warnings and fix error

--- soem/ethercatfoe.c
+++ soem/ethercatfoe.c
@@ -86,7 +86,7 @@ int ecx_FOEread(ecx_contextt *context, uint16 slave, char *filename, uint32 pass
    ec_clearmbx(&MbxOut);
    aFOEp = (ec_FOEt *)&MbxIn;
    FOEp = (ec_FOEt *)&MbxOut;
-   fnsize = strlen(filename);
+   fnsize = (uint16)strlen(filename);
    maxdata = context->slavelist[slave].mbx_l - 12;
    if (fnsize > maxdata)
    {
@@ -215,7 +215,7 @@ int ecx_FOEwrite(ecx_contextt *context, uint16 slave, char *filename, uint32 pas
    aFOEp = (ec_FOEt *)&MbxIn;
    FOEp = (ec_FOEt *)&MbxOut;
    dofinalzero = FALSE;
-   fnsize = strlen(filename);
+   fnsize = (uint16)strlen(filename);
    maxdata = context->slavelist[slave].mbx_l - 12;
    if (fnsize > maxdata)
    {

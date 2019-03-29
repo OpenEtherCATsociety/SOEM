a448749ff715e9c263c99f2c3019933bc2d4a2aa
Davide Pippa <thepyper@gmail.com>
2019-03-29 10:53:36 +0100

    Better build script takes stddef.h path as argument

--- build_rpi_3.sh
+++ build_rpi_3.sh
@@ -1,14 +1,17 @@
-#export ARM_GCC_DIR=/h/Qt/gcc-arm-none-eabi-8-2018-q4-major-win32
-#export ARM_GCC_STDDEF_H_DIR=/h/Qt/gcc-arm-none-eabi-8-2018-q4-major-win32/lib/gcc/arm-none-eabi/8.2.1/include
 
-# execute in a MSYS2-MINGW32 shell
+#execute in a MSYS2-MINGW32 shell, with arm-none-eabi-gcc already in path.
+#export PATH=$PATH:/h/Qt/gcc-arm-none-eabi-8-2018-q4-major-win32/bin
+
+if [ "$1" == "" ]; then
+	#/h/Qt/gcc-arm-none-eabi-8-2018-q4-major-win32/lib/gcc/arm-none-eabi/8.2.1/include
+    echo "Usage: build_rpi_3.sh <path to stddef.h>"
+	exit
+fi
 
 export HOMEDIR=$PWD
 
-export PATH=$PATH:$ARM_GCC_DIR/bin
-
 cd $HOMEDIR/libs/circle-stdlib
-./configure -r 3 -s $ARM_GCC_STDDEF_H_DIR
+./configure -r 3 -s $1
 make
 
 cd $HOMEDIR
